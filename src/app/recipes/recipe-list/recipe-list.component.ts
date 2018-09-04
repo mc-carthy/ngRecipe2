50572d08f5b74c4de669375cf4f548d6d36472c4
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-04 21:25:44 +0100

    Add recipe item template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-    recipes = [];
+    recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'Test description', 'https://cdn.pixabay.com/photo/2017/04/04/00/36/japanese-food-2199962_960_720.jpg')
+    ];
 
     constructor() { }
 

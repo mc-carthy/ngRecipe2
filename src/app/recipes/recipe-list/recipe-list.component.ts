648e7449932f8aeecc2677970c61d63ba9923776
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-12 20:39:22 +0100

    Passing recipe data from recipe-list to recipe-item component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from './../recipe.model';
 export class RecipeListComponent implements OnInit {
 
     recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'Test description', 'https://cdn.pixabay.com/photo/2017/04/04/00/36/japanese-food-2199962_960_720.jpg')
+        new Recipe('Test Recipe', 'Test description', 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Indonesian_Food.png'),
+        new Recipe('Test Recipe', 'Test description', 'https://upload.wikimedia.org/wikipedia/commons/5/58/Korean.food-Yukhoe-02E.jpg'),
     ];
 
     constructor() { }

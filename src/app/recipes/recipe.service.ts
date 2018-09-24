fd2ecd0c84d76b856a730920d413c6b3fe404376
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-24 23:02:30 +0100

    Use services to facilitate cross-component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
     
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Test Recipe', 'Test description', 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Indonesian_Food.png'),
         new Recipe('Test Recipe', 'Test description', 'https://upload.wikimedia.org/wikipedia/commons/5/58/Korean.food-Yukhoe-02E.jpg'),
@@ -9,5 +12,7 @@ export class RecipeService {
 
     getRecipes() {
         return this.recipes.slice();
-    }
+    };
+
+
 }

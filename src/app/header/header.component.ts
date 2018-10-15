859724a6c2ddfd44df7db903323bac3e8ffa3b5e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-15 21:30:48 +0100

    Setup routing in navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,16 +6,9 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-    @Output() featureSelected = new EventEmitter<string>()
-
     constructor() { }
 
     ngOnInit() {
-    }
-
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
     }
 
 }

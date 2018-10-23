0bd762df3f307717366b1b74af6f9531d7b56e51
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-23 21:59:31 +0100

    Add new ingredient controls to new/edit recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -59,4 +59,13 @@ export class RecipeEditComponent implements OnInit {
         console.log(this.recipeForm);
     }
 
+    onAddIngredient() {
+        (<FormArray>this.recipeForm.get('ingredients')).push(
+            new FormGroup({
+                'name': new FormControl(null),
+                'amount': new FormControl(null)
+            })
+        );
+    }
+
 }

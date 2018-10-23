2b4ed5c48412bf8dccdb2810024ce398a8ddacc3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-23 23:05:40 +0100

    Remember to unsubscribe from custom subscriptions...

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from './../recipe.model';
 import { RecipeService } from './../recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
+    subscription = new Subscription;
     recipes: Recipe[]
 
     constructor(private recipeService: RecipeService) { }
 
     ngOnInit() {
-        this.recipeService.recipesChanged.subscribe(
+        this.subscription = this.recipeService.recipesChanged.subscribe(
             (recipes: Recipe[]) => {
                 this.recipes = recipes;
             }
         );
         this.recipes = this.recipeService.getRecipes()
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
 }

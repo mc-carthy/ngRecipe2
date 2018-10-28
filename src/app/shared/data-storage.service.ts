50135fd5d36aa71c9d3c82952b90f644d5450ee5
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-28 12:41:16 +0000

    Transform data received from server to ensure it fits our model

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 import { RecipeService } from './../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 
@@ -15,9 +16,19 @@ export class DataStorageService {
     }
 
     getRecipes() {
-        this.http.get('https://ngrecipe-25ef8.firebaseio.com/recipes.json').subscribe(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json();
+        this.http.get('https://ngrecipe-25ef8.firebaseio.com/recipes.json').pipe(map(
+        (response: Response) => {
+            const recipes: Recipe[] = response.json();
+            for (let recipe of recipes) {
+                if (!recipe.ingredients) {
+                    console.log(recipe);
+                    recipe['ingredients'] = [];
+                }
+            }
+            return recipes;
+        }
+        )).subscribe(
+            (recipes: Recipe[]) => {
                 this.recipeService.setRecipes(recipes);
             }
         );

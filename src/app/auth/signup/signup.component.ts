3aec2645a60a9f6ad2a1b6efa6e145cf403b19d2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-28 13:43:50 +0000

    Signing in with firebase

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -18,7 +18,6 @@ export class SignupComponent implements OnInit {
         const email = form.value.email;
         const password = form.value.password;
         this.authService.signupUser(email, password);
-        console.log('Clicked signup');
     }
 
 }

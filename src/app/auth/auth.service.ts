4ea6178b5648532f37c4034bcb20d778e30d4e2c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-28 17:15:02 +0000

    Redirect to home upon signin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
+@Injectable()
 export class AuthService {
     token: string;
+
+    constructor(private router: Router) {}
 
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -15,6 +20,7 @@ export class AuthService {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
                 response => {
+                    this.router.navigate(['/']);
                     firebase.auth().currentUser.getIdToken()
                         .then(
                             (token: string) => this.token = token

ebbc6bf261e34c6659ed24b301f9a5be5669166d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-28 21:19:59 +0000

    Create shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,12 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
-import { DropdownDirective } from './shared/dropdown.directive';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
@@ -26,7 +25,6 @@ import { SigninComponent } from './auth/signin/signin.component';
         HeaderComponent,
         ShoppingListComponent,
         ShoppingEditComponent,
-        DropdownDirective,
         SignupComponent,
         SigninComponent
     ],
@@ -35,7 +33,8 @@ import { SigninComponent } from './auth/signin/signin.component';
         HttpModule,
         FormsModule,
         RecipesModule,
-        AppRoutingModule
+        AppRoutingModule,
+        SharedModule
     ],
     providers: [
         ShoppingListService,
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from './../shared/shared.module';
+
 
 import { RecipesComponent } from './recipes.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
@@ -23,7 +24,8 @@ import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.compo
     imports: [
         CommonModule,
         ReactiveFormsModule,
-        RecipesRoutingModule
+        RecipesRoutingModule,
+        SharedModule
     ]
 })
 export class RecipesModule {
